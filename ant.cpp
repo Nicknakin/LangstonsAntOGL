33b87bebd9c7ba92ec671df70d112399c58f3ff1
= <Kinneyanick@gmail.com>
2018-09-20 16:43:03 -0500

    Added ant class

---
+++ ant.cpp
@@ -0,0 +1,71 @@
+#include "ant.h"
+
+Ant::Ant(int x, int y, int direction, float r, float g, float b): sf::CircleShape{3}, x{x}, y{y}, direction{direction}, r{r}, g{g}, b{b}, id{ID++} {
+    setFillColor(sf::Color{(int) (r*255), (int) (g*255), (int) (b*255)});
+    setRotation(90*direction);
+
+}
+
+void Ant::setPosition(int xMultiplier, int yMultiplier){
+    setPosition(x*xMultiplier, y*yMultiplier);
+    setRotation(90*direction);
+}
+
+void Ant::wrap(int xMax, int yMax){
+    x = (x+xMax)%xMax;
+    y = (y+yMax)%yMax;
+}
+
+void Ant::move(Cell cell){
+    if(&cell == nullptr)
+        return;
+
+    if(cell.getValue())
+        turnLeft();
+    else
+        turnRight();
+    cell.setValue(1-cell.getValue());
+    cell.setColor(r*255, g*255, b*255);
+
+    x += (direction == 1)? 1: (direction == 3)? -1: 0;
+    y += (direction == 0)? -1: (direction == 2)? 1: 0;
+}
+
+int Ant::getID(){
+    return id;
+}
+
+int Ant::getX(){
+    return x;
+}
+
+int Ant::getY(){
+    return y;
+}
+
+int Ant::getDirection(){
+    return direction;
+}
+
+float Ant::getR(){
+    return r;
+}
+
+float Ant::getG(){
+    return b;
+}
+
+float Ant::getB(){
+    return b;
+}
+
+void Ant::turnLeft(){
+    direction += 3;
+    direction %= 4;
+}
+
+void Ant::turnRight(){
+    direction += 1;
+    direction %= 4;
+}
+        
---
+++ ant.h
@@ -0,0 +1,36 @@
+#ifndef antinc
+#define antinc
+
+#include <SFML/Graphics.hpp>
+
+#include "cell.h"
+
+class Ant: public sf::CircleShape {
+    public:
+        Ant(int x, int y, int direction, float r, float g, float b);
+        void setPosition(int xMultiplier, int yMultiplier);
+        void wrap(int xMax, int yMax);
+        void move(Cell cell);
+
+        int getID();
+        int getX();
+        int getY();
+        int getDirection();
+        float getR();
+        float getG();
+        float getB();
+
+    private:
+        void turnLeft();
+        void turnRight();
+        
+        static int ID;
+        int x, y, direction;
+        float r, g, b;
+        const int id;
+
+};
+
+static int ID = 0;
+
+#endif
